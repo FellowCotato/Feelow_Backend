3885cada275d0bbe1f287a8ccb72eedeeea462e3
nar0ng <71819594+nar0ng@users.noreply.github.com>
2023-12-18 22:35:37 +0900

    Revert "로그인 기능 구현"

--- Feelow/src/main/java/com/feelow/Feelow/jwt/MemberInfo.java
+++
@@ -1,9 +0,0 @@
-package com.feelow.Feelow.jwt;
-
-import lombok.Value;
-
-@Value
-public class MemberInfo {
-    private String email;
-    private String nickname;
-}
--- Feelow/src/main/java/com/feelow/Feelow/repository/MemberRepository.java
+++ Feelow/src/main/java/com/feelow/Feelow/repository/MemberRepository.java
@@ -1,15 +1,7 @@
 package com.feelow.Feelow.repository;
 
-import com.feelow.Feelow.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import com.feelow.Feelow.entity.Member;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    // ID로 member 찾기
-    Optional<Member> findById(Long id);
-
+public interface MemberRepository extends CrudRepository<Member, Long> {
 }
